389e0fbad0ffae8e968657e98193a9c05a676fb7
L-PDufour <leonpierre.dufour@gmail.com>
2025-06-02 18:37:54 -0400

    companion

--- lua/user/others.lua
+++ lua/user/others.lua
@@ -1,4 +1,33 @@
--- Check if snacks is available (in case it's not installed yet)
+local api_key_cache = nil
+
+require("codecompanion").setup({
+  adapters = {
+    anthropic = function()
+      return require("codecompanion.adapters").extend("anthropic", {
+        env = {
+          api_key = function()
+            if not api_key_cache then
+              api_key_cache = vim.fn.inputsecret("Enter Claude API key: ")
+            end
+            return api_key_cache
+          end,
+        },
+      })
+    end,
+  },
+  strategies = {
+    chat = { adapter = "anthropic" },
+    inline = { adapter = "anthropic" },
+    cmd = { adapter = "anthropic" }
+  },
+})
+
+vim.keymap.set({ "n", "v" }, "<C-a>", "<cmd>CodeCompanionActions<cr>", { noremap = true, silent = true })
+vim.keymap.set({ "n", "v" }, "<LocalLeader>a", "<cmd>CodeCompanionChat Toggle<cr>", { noremap = true, silent = true })
+vim.keymap.set("v", "ga", "<cmd>CodeCompanionChat Add<cr>", { noremap = true, silent = true })
+
+-- Expand 'cc' into 'CodeCompanion' in the command line
+vim.cmd([[cab cc CodeCompanion]])
 local harpoon = require("harpoon")
 require("lazydev").setup({ ft = "lua" })
 harpoon:setup()
