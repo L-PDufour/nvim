9eae8afd455a964b6503fc7ecb6b5c291a60153a
Léon-Pierre Dufour <leonpierre.dufour@gmail.com>
2026-05-23 17:29:13 -0400

    fix void issue

--- flake.nix
+++ flake.nix
@@ -1,41 +1,42 @@
 {
   description = "neovim — self-contained, Nix-managed";
+
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-  outputs =
-    { self, nixpkgs }:
+
+  outputs = { self, nixpkgs }:
     let
-      systems = [
-        "x86_64-linux"
-        "aarch64-linux"
-      ];
+      systems      = [ "x86_64-linux" "aarch64-linux" ];
       forAllSystems = nixpkgs.lib.genAttrs systems;
-      pkgsFor =
-        system:
-        import nixpkgs {
-          inherit system;
-          config.allowUnfree = true;
-        };
-    in
-    {
-      lib = import ./lib {
-        inputs = self.inputs;
-        inherit pkgsFor;
+      pkgsFor      = system: import nixpkgs {
+        inherit system;
+        config.allowUnfree = true;
       };
+    in {
+      lib = import ./lib { inputs = self.inputs; inherit pkgsFor; };
+
       packages = forAllSystems (system: {
-        default = self.lib.mkVimPlugin { inherit system; };
-        neovim = self.lib.mkNeovim { inherit system; };
+        default   = self.lib.mkNeovim { inherit system; };
+        neovim    = self.lib.mkNeovim { inherit system; };
+        vimPlugin = self.lib.mkVimPlugin { inherit system; };
       });
+
       apps = forAllSystems (system: {
-        nvim = {
+        default = {
+          type    = "app";
           program = "${self.packages.${system}.neovim}/bin/nvim";
-          type = "app";
+        };
+        nvim = {
+          type    = "app";
+          program = "${self.packages.${system}.neovim}/bin/nvim";
         };
       });
+
       devShells = forAllSystems (system: {
         default = (pkgsFor system).mkShell {
           buildInputs = [ (pkgsFor system).just ];
         };
       });
+
       formatter = forAllSystems (system: (pkgsFor system).alejandra);
     };
 }
